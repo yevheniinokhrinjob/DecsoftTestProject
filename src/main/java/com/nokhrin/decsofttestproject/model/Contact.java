648d59b282db6c35c005aebc3d2062dff8266bbe
yevheniinokhrin <233315@edu.p.lodz.pl>
2022-04-01 16:08:18 +0200

    created contact model

---
+++ src/main/java/com/nokhrin/decsofttestproject/model/Contact.java
@@ -0,0 +1,18 @@
+package com.nokhrin.decsofttestproject.model;
+
+
+public class Contact {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private int homePhoneNumber;
+
+    private int workPhoneNumber;
+
+    private String email;
+
+}
