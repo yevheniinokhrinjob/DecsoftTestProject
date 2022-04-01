26149a555a3e0d4b90890148d785545af34f74e3
yevheniinokhrin <233315@edu.p.lodz.pl>
2022-04-01 13:57:13 +0200

    first commit

---
+++ src/main/java/com/nokhrin/decsofttestproject/DecsofttestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.nokhrin.decsofttestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecsofttestprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecsofttestprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nokhrin/decsofttestproject/DecsofttestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokhrin.decsofttestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecsofttestprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
